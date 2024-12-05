4afebcc7cc91671d5dcdef7543269b05a1397291
Marcus <marcus.henriksson@restory.ai>
2024-12-05 19:32:04 +0100

    changed DB server host

--- dbConnect.js
+++ dbConnect.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize({
   database: 'medie',
   username: 'root',
   password: 'SokrateS13',
-  host: 'localhost',
+  host: 'medieinsitutetdb.cjqg0ggk63v9.eu-north-1.rds.amazonaws.com',
   dialect: 'mysql', // Specificerar vilken databas vi jobbar med
 });
 
